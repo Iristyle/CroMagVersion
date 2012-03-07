a5288185372cc58d147e2b7593bb20c74901ce27
Ethan J. Brown <ethan.j.brown@gmail.com>
2012-03-07 15:39:47 -0500

    this is the main file that gets shared in multiple projects (but that gets overwritten on each build / script run)

---
+++ tools/SharedAssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+// DON'T modify this file -- its modified by an automated build script
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
