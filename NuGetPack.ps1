8ac919c300f32f46079a2b336d0d95189a55f3ef
Ethan J. Brown <ethan.j.brown@gmail.com>
2012-03-07 16:43:39 -0500

    much smarter packing script
    - broke out directory sniffing to its own method
    - added a call to restore NugetRestore.proj via an msbuild call (basically brings down nuget.exe when you intend to push)
    - directory sniffer is much smarter / faster - uses filter, ignores packages dir and packs up differently based on whether or not theres a corresponding csproj hanging around

--- NuGetPack.ps1
+++ NuGetPack.ps1
@@ -1,23 +1,63 @@
 param(
 	[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
 	[string]
-	$apiKey
+	$apiKey,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $source = ''
 )
+
+function Get-CurrentDirectory
+{
+    $thisName = $MyInvocation.MyCommand.Name
+    [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
+function Get-NugetPath
+{
+    Write-Host "Executing Get-NugetPath"
+    Get-ChildItem -Path (Get-CurrentDirectory) -Include 'nuget.exe' -Recurse |
+        Select -ExpandProperty FullName -First 1
+}
+
+function Restore-Nuget
+{
+    Write-Host "Executing Restore-Nuget"
+    $nuget = Get-NugetPath
+
+    if ($nuget -ne $null)
+    {
+        return $nuget        
+    }
+
+    #$msbuild = "c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+    Start-Process 'msbuild' -ArgumentList "NugetRestore.proj" -WorkingDirectory (Join-Path (Get-CurrentDirectory) '.nuget') -NoNewWindow -Wait
+    Get-NugetPath
+}
 
 function Pack-And-Push
 {
-    $thisName = $MyInvocation.MyCommand.Name
-    $currentDirectory = [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
-    Write-Host "Running against $currentDirectory"
-    $nuget = Get-ChildItem -Path $currentDirectory -Include 'nuget.exe' -Recurse |
-        Select -ExpandProperty FullName -First 1
+    $currentDirectory = Get-CurrentDirectory
+    Write-Host "Running against $currentDirectory"    
 
-    Get-ChildItem -Path $currentDirectory -Include *.nuspec -Recurse | 
-        % { Join-Path ([IO.Path]::GetDirectoryName($_)) ([IO.Path]::GetFileNameWithoutExtension($_) + '.csproj') } |
-        ? { Test-Path $_ } |
-        % { Start-Process $nuget -ArgumentList "pack $_ -Build -Prop Configuration=Release -Exclude '**\*.CodeAnalysisLog.xml'" -NoNewWindow -Wait }
+    $nuget = Restore-Nuget
+
+    Get-ChildItem -Path $currentDirectory -Filter *.nuspec -Recurse | 
+        ? { $_.FullName -inotmatch 'packages' } |
+        % {
+            $csproj = Join-Path $_.DirectoryName ($_.BaseName + '.csproj')
+            $cmdLine = if (Test-Path $csproj) { "pack $csproj -Prop Configuration=Release -Exclude '**\*.CodeAnalysisLog.xml'" }
+            else { "pack $_"}
+            
+            Start-Process $nuget -ArgumentList $cmdLine -NoNewWindow -Wait
+        }
     
-    Get-ChildItem *.nupkg | % { &$nuget push $_ $apiKey }     
+    Get-ChildItem *.nupkg | 
+        % { 
+            Write-Host "Value of source -> $source"
+            if ($source -eq '') { &$nuget push $_ $apiKey }
+            else { &$nuget push $_ $apiKey -s $source }            
+        }
 }
 
 del *.nupkg
